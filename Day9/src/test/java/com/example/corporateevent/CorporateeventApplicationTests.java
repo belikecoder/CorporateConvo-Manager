b42b17e6d994741b2bfe08b91d734c021184cc46
VIGNESH P <113431985+belikecoder@users.noreply.github.com>
2024-02-08 22:36:18 +0530

    day 9 backend

---
+++ Day9/src/test/java/com/example/corporateevent/CorporateeventApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.corporateevent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorporateeventApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
