2ece64d95c01dd3424ea08e1a8cbfe066f1569a4
VIGNESH P <113431985+belikecoder@users.noreply.github.com>
2024-02-08 22:34:33 +0530

    day7 backend

---
+++ Day7/src/main/java/com/example/corporateevent/CorporateeventApplication.java
@@ -0,0 +1,13 @@
+package com.example.corporateevent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorporateeventApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorporateeventApplication.class, args);
+	}
+
+}
